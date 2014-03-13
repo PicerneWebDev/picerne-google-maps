08099bdcb8d90164ebc3c67f3f00e8741f029676
Jonathan Rivera - Picerne <jrivera@picernefl.com>
2014-03-13 09:46:20 -0400

    Setup Laurel Point POI's

--- js/map-settings.js
+++ js/map-settings.js
@@ -19,13 +19,13 @@ jQuery(function($)
    });
    
    $('#map_addresses').gMap({
-        address: "9465 Clearwood St. Houston, Texas",
+        address: "16170 Westpark Drive Houston, Texas",
         zoom: 13,
 		 markers:[
 			{
-				latitude: 29.6336447,
-				longitude: -95.2454074,
-				html: "<strong>Clearwood Villas Apartment Homes</strong><p>9465 Clearwood St<br>Houston, TX 77075</p>",
+				latitude: 29.7112569,
+				longitude: -95.6684441,
+				html: "<strong>Laurel Point Senior Apartment Homes</strong><p>16170 Westpark Dr<br>Houston, TX 77082</p>",
 				icon: {
 					image: "/plugins/picerne-google-maps/img/gmap_pin_home.png",
 					iconsize: [40,40],
@@ -35,9 +35,9 @@ jQuery(function($)
 				
 			},
 			{
-			    latitude: 29.6024159,
-				longitude: -95.2484664,
-				html: "<strong>H-E-B Grocery Store</strong><p>9828 Blackhawk Blvd.<br>Houston, TX 77075</p>",
+			    latitude: 29.708517,
+				longitude: -95.6864,
+				html: "<strong>Kroger Grocery Store</strong><p>6425 FM 1464 Rd<br>Richmond, TX 77407<br>Phone: (281) 277-5353</p>",
 				icon: {
 					image: "/plugins/picerne-google-maps/img/gmap_pin_grocery.png",
 					iconsize: [20,20],
@@ -45,9 +45,9 @@ jQuery(function($)
 				}	
 			},
 			{
-			    latitude: 29.641843,
-				longitude: -95.2422658,
-				html: "<strong>Fiesta Food Mart</strong><p>1175 Edgebrook Drive<br>Houston, TX 77034</p>",
+			    latitude: 29.703955,
+				longitude: -95.6382774,
+				html: "<strong>Fiesta Food Mart</strong><p>14315 Bellaire Blvd<br>Houston, TX 77083</p>",
 				icon: {
 					image: "/plugins/picerne-google-maps/img/gmap_pin_grocery.png",
 					iconsize: [20,20],
@@ -55,61 +55,71 @@ jQuery(function($)
 				}	
 			},
 			{
-			    latitude: 29.6399528,
-				longitude: -95.2453165,
-				html: "<strong>First Service Credit Union</strong><p>8980 Clearwood Drive<br>Houston, TX 77075</p>"						
-			},
-			{
-			    latitude: 29.6308159,
-				longitude: -95.2362936,
-				html: "<strong>Walmart SuperCenter</strong><p>9598 Rowlett Rd.<br>Houston, TX 77075</p>",
+			    latitude: 29.7211391,
+				longitude: -95.6467403,
+				html: "<strong>Walmart SuperCenter</strong><p>3506 Hwy 6 S<br>Houston, TX 77082<br>(281) 313-0111</p>",
 				icon: {
 					image: "/plugins/picerne-google-maps/img/gmap_pin_grocery.png",
 					iconsize: [20,20],
 					iconanchor: [20,20]
 				}					
+			},			
+			{
+			    latitude: 29.7096736,
+				longitude: -95.6871699,
+				html: "<strong>Chase Bank</strong><p>17225 Westheimer Rd<br>Houston, TX 77082</p>"						
 			},
 			{
-			    latitude: 29.6403636,
-				longitude: -95.2442206,
-				html: "<strong>McDonald's</strong><p>8920 Clearwood St.<br>Houston, TX 77075</p>"				
+			    latitude: 29.6924171,
+				longitude: -95.5874705,
+				html: "<strong>Henington-Alief Regional Library</strong><p>7979 South Kirkwood<br>Houston, TX 77072</p>"				
 			},
 			{
-			    latitude: 29.628009,
-				longitude: -95.2504907,
-				html: "<strong>Jessup Elementary School</strong><p>9301 Almeda Genoa Rd<br>Houston, TX 77075</p>",
+			    latitude: 29.7340216,
+				longitude: -95.7647426,
+				html: "<strong>YMCA</strong><p>22807 Westheimer Pkwy<br>Katy, TX 77494 <br>(281) 392-5055</p>"				
+			},
+			{
+			    latitude: 29.6172405,
+				longitude: -95.637097,
+				html: "<strong>T.E. Harman Center - Senior Center</strong><p>226 Matlage Way<br>Sugar Land, TX 77478 <br>(281) 275-2893</p>"				
+			},	
+			{
+			    latitude: 29.7203405,
+				longitude: -95.5746978,
+				html: "<strong>Metropolitan Transit Authority Bus Station</strong><p>11555 Westpark Drive<br>Houston, TX 77082 <br>(281) 589-8461</p>"				
+			},
+			{
+			    latitude: 29.7458821,
+				longitude: -95.6085678,
+				html: "<strong>Ashford West US Post Office</strong><p>12655 Whittington Dr.<br>Houston, TX 77077 <br>(800) 275-8777</p>"				
+			},										
+			{
+			    latitude: 29.6933076,
+				longitude: -95.522261,
+				html: "<strong>Memorial Hermann Southwest Hospital</strong><p>7600 Beechnut St<br>Houston, TX 77074<br>(713) 456-5000</p>",
 				icon: {
-					image: "/plugins/picerne-google-maps/img/gmap_pin_school.png",
+					image: "/plugins/picerne-google-maps/img/gmap_pin_hospital.png",
 					iconsize: [20,20],
 					iconanchor: [20,20]
 				}
 			},
 			{
-			    latitude: 29.6477003,
-				longitude: -95.2491267,
-				html: "<strong>Rick Schneider Middle School</strong><p>8420 Easthaven Blvd.<br>Houston, TX 77075</p>",
+			    latitude: 29.7871193,
+				longitude: -95.6968982,
+				html: "<strong>Houston Methodist West Hospital</strong><p>18500 Katy Freeway<br>Houston, TX 77094<br>(832) 522-1000</p>",
 				icon: {
-					image: "/plugins/picerne-google-maps/img/gmap_pin_school.png",
+					image: "/plugins/picerne-google-maps/img/gmap_pin_hospital.png",
 					iconsize: [20,20],
 					iconanchor: [20,20]
 				}
 			},
 			{
-			    latitude: 29.6582508,
-				longitude: -95.2421983,
-				html: "<strong>South Houston Intermediate School</strong><p>900 College Ave.<br>South Houston, TX 77587</p>",
+			    latitude: 29.729133,
+				longitude: -95.59429,
+				html: "<strong>West Houston Medical Center</strong><p>12141 Richmond Ave.<br>Houston, TX 77082<br>(281) 558-3444</p>",
 				icon: {
-					image: "/plugins/picerne-google-maps/img/gmap_pin_school.png",
-					iconsize: [20,20],
-					iconanchor: [20,20]
-				}
-			},
-			{
-			    latitude: 29.6522554,
-				longitude: -95.2122274,
-				html: "<strong>South Houston High School</strong><p>3820 Shaver St.<br>South Houston, TX 77587</p>",
-				icon: {
-					image: "/plugins/picerne-google-maps/img/gmap_pin_school.png",
+					image: "/plugins/picerne-google-maps/img/gmap_pin_hospital.png",
 					iconsize: [20,20],
 					iconanchor: [20,20]
 				}

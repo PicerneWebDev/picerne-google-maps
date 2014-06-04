3ed19d511c3e7c7dfa96e5efdfde20e4fdbb80aa
Jonathan Rivera - Picerne <jrivera@picernefl.com>
2014-06-04 13:30:38 -0400

    Add Publix and USPS pins

--- js/map-settings.js
+++ js/map-settings.js
@@ -7,7 +7,7 @@ jQuery(function($)
         latitude: -2.206,
         longitude: -79.897,
         maptype: 'TERRAIN', // 'HYBRID', 'SATELLITE', 'ROADMAP' or 'TERRAIN'
-        zoom: 8,
+        zoom: 7,
         controls: {
             panControl: true,
             zoomControl: false,
@@ -19,13 +19,13 @@ jQuery(function($)
    });
    
    $('#map_addresses').gMap({
-        address: "518 Lake Bridge Lane Apopka, Arizona",
-        zoom: 13,
+        address: "614 Wes Kearney Way Riverview, Florida",
+        zoom: 12,
 		 markers:[
 			{
-				latitude: 28.6701259,
-				longitude: -81.4695266,
-				html: "<strong>The Oasis at Wekiva Apartment Homes</strong><p>518 Lake Bridge Lane<br>Apopka, FL 32703</p><a href=\"//facebook.com/oasisatwekiva\" target=\"_blank\">Like us on Facebook</a>",
+				latitude: 27.9175047,
+				longitude: -82.3446241,
+				html: "<strong>The Oasis at Brandon Luxury Apartment Homes</strong><p>9503 Oasis Way Blvd<br>Riverview, FL 33578</p><a href=\"//facebook.com/oasisatbrandon\" target=\"_blank\">Like us on Facebook</a>",
 				icon: {
 					image: "/plugins/picerne-google-maps/img/gmap_pin_oasis.png",
 					iconsize: [26,46],
@@ -35,9 +35,39 @@ jQuery(function($)
 				
 			},
 			{
-			    latitude: 28.6693296,
-				longitude: -81.4423346,
-				html: "<strong>CareSpot Urgent Care Apopka</strong><p>3840 Florida 436 #1000<br>Apopka, FL 32703</p>",
+			    latitude: 27.912579,
+				longitude: -82.348378,
+				html: "<strong>Publix Super Market</strong><p>3863 U.S. 301<br>Riverview, FL 33578</p>",
+				icon: {
+					image: "/plugins/picerne-google-maps/img/gmap_pin_publix.png",
+					iconsize: [20,20],
+					iconanchor: [20,20]
+				}
+			},
+			{
+			    latitude: 27.925068,
+				longitude: -82.32484,
+				html: "<strong>Wal-Mart Supercenter</strong><p>3863 U.S. 301<br>Riverview, FL 33578</p>",
+				icon: {
+					image: "/plugins/picerne-google-maps/img/gmap_pin_grocery.png",
+					iconsize: [20,20],
+					iconanchor: [20,20]
+				}
+			},
+			{
+			    latitude: 27.8930747,
+				longitude: -82.3187212,
+				html: "<strong>Walgreens</strong><p>11010 Bloomingdale Ave.<br>Riverview, FL 33578</p>",
+				icon: {
+					image: "/plugins/picerne-google-maps/img/gmap_pin_grocery.png",
+					iconsize: [20,20],
+					iconanchor: [20,20]
+				}
+			},			
+			{
+			    latitude: 27.932589,
+				longitude: -82.28759,
+				html: "<strong>Brandon Regional Hospital</strong><p>119 Oakfield Dr,<br>Brandon, FL 33511</p>",
 				icon: {
 					image: "/plugins/picerne-google-maps/img/gmap_pin_hospital.png",
 					iconsize: [20,20],
@@ -45,39 +75,9 @@ jQuery(function($)
 				}
 			},
 			{
-			    latitude: 28.6809867,
-				longitude: -81.5084757,
-				html: "<strong>Florida Hospital Apopka</strong><p>201 N. Park Ave.<br>Apopka, FL 32703</p>",
-				icon: {
-					image: "/plugins/picerne-google-maps/img/gmap_pin_hospital.png",
-					iconsize: [20,20],
-					iconanchor: [20,20]
-				}				
-			},
-			{
-			    latitude: 28.6504411,
-				longitude: -81.4691204,
-				html: "<strong>Walmart Supercenter</strong><p>1700 South Orange Blossom Tr.<br>Apopka, FL 32703</p>",
-				icon: {
-					image: "/plugins/picerne-google-maps/img/gmap_pin_grocery.png",
-					iconsize: [20,20],
-					iconanchor: [20,20]
-				}	
-			},
-			{
-			    latitude: 28.6710567,
-				longitude: -81.4496284,
-				html: "<strong>Publix Super Market</strong><p>540 South Hunt Club Blvd.<br>Apopka, FL 32703</p>",
-				icon: {
-					image: "/plugins/picerne-google-maps/img/gmap_pin_grocery.png",
-					iconsize: [20,20],
-					iconanchor: [20,20]
-				}	
-			},
-			{
-			    latitude: 28.6719824,
-				longitude: -81.5080495,
-				html: "<strong>Apopka Fire Department</strong><p>175 East 5th St.<br>Apopka, FL 32703</p>",
+			    latitude: 27.918988,
+				longitude: -82.368613,
+				html: "<strong>Hillsborough County Fire Department</strong><p>3210 S. 78th St.<br>Tampa, FL 33619</p>",
 				icon: {
 					image: "/plugins/picerne-google-maps/img/gmap_pin_fire.png",
 					iconsize: [20,20],
@@ -85,19 +85,19 @@ jQuery(function($)
 				}						
 			},
 			{
-			    latitude: 28.6679073,
-				longitude: -81.4677144,
-				html: "<strong>Apopka Fire Station</strong><p>801 Piedmont Wekiva Road<br>Apopka, FL 32703</p>",
+			    latitude: 27.8924866,
+				longitude: -82.2949887,
+				html: "<strong>Post Office</strong><p>867 W. Bloomingdale Ave<br>Brandon, FL 33511</p>",
 				icon: {
-					image: "/plugins/picerne-google-maps/img/gmap_pin_fire.png",
+					image: "/plugins/picerne-google-maps/img/gmap_pin_usps.png",
 					iconsize: [20,20],
 					iconanchor: [20,20]
 				}					
 			},
 			{
-			    latitude: 28.6705178,
-				longitude: -81.5086442,
-				html: "<strong>Apopka Police Station</strong><p>112 East 6th St.<br>Apopka, FL 32703</p>",
+			    latitude: 27.96429,
+				longitude: -82.336328,
+				html: "<strong>Hillsborough County Sheriff's Office</strong><p>2310 N Falkenburg Rd.<br>Tampa, FL 33619</p>",
 				icon: {
 					image: "/plugins/picerne-google-maps/img/gmap_pin_police.png",
 					iconsize: [20,20],
@@ -105,9 +105,9 @@ jQuery(function($)
 				}						
 			},
 			{
-			    latitude: 28.6683312,
-				longitude: -81.483127,
-				html: "<strong>Lovell Elementary School</strong><p>815 Roger Williams Road.<br>Apopka, FL 32703</p>",
+			    latitude: 27.889314,
+				longitude: -82.317586,
+				html: "<strong>Winthrop Charter School</strong><p>6204 Scholars Hill Ln<br>Riverview, FL 33578</p>",
 				icon: {
 					image: "/plugins/picerne-google-maps/img/gmap_pin_school.png",
 					iconsize: [20,20],
@@ -115,9 +115,9 @@ jQuery(function($)
 				}
 			},
 			{
-			    latitude: 28.6375826,
-				longitude: -81.4770176,
-				html: "<strong>Piedmont Lakes Middle School</strong><p>2601 Lakeville Road<br>Apopka, FL 32703</p>",
+			    latitude: 27.873993,
+				longitude: -82.324872,
+				html: "<strong>Riverview Elementary</strong><p>10809 Hannaway Dr.<br>Riverview, FL 33569</p>",
 				icon: {
 					image: "/plugins/picerne-google-maps/img/gmap_pin_school.png",
 					iconsize: [20,20],
@@ -125,15 +125,75 @@ jQuery(function($)
 				}
 			},
 			{
-			    latitude: 28.6381769,
-				longitude: -81.4736576,
-				html: "<strong>Wekiva High School</strong><p>2501 N. Hiawassee Road<br>Apopka, FL 32703</p>",
+			    latitude: 27.8754606,
+				longitude: -82.3272109,
+				html: "<strong>Guinta Middle School</strong><p>4202 S. Falkenburg Rd.<br>Riverview, FL 33578</p>",
 				icon: {
 					image: "/plugins/picerne-google-maps/img/gmap_pin_school.png",
 					iconsize: [20,20],
 					iconanchor: [20,20]
 				}
-			}					
+			},
+			{
+			    latitude: 27.884483,
+				longitude: -82.352188,
+				html: "<strong>Spoto High School</strong><p>8538 Eagle Palm Dr.<br>Riverview, FL 33578</p>",
+				icon: {
+					image: "/plugins/picerne-google-maps/img/gmap_pin_school.png",
+					iconsize: [20,20],
+					iconanchor: [20,20]
+				}
+			},
+			{
+			    latitude: 27.9650212,
+				longitude: -82.4350941,
+				html: "<strong>Ybor City</strong><p>Tampa, FL 33605</p>",
+				icon: {
+					image: "/plugins/picerne-google-maps/img/gmap_pin.png",
+					iconsize: [20,20],
+					iconanchor: [20,20]
+				}
+			},
+			{
+			    latitude: 27.950131,
+				longitude: -82.4465508,
+				html: "<strong>Channel District</strong><p>Tampa, FL 33605</p>",
+				icon: {
+					image: "/plugins/picerne-google-maps/img/gmap_pin.png",
+					iconsize: [20,20],
+					iconanchor: [20,20]
+				}
+			},
+			{
+			    latitude: 27.948284,
+				longitude: -82.4568279,
+				html: "<strong>Downtown Tampa District</strong><p>Tampa, FL 33605</p>",
+				icon: {
+					image: "/plugins/picerne-google-maps/img/gmap_pin.png",
+					iconsize: [20,20],
+					iconanchor: [20,20]
+				}
+			},
+			{
+			    latitude: 27.9332974,
+				longitude: -82.4718781,
+				html: "<strong>Hyde Park</strong><p>Tampa, FL 33606</p>",
+				icon: {
+					image: "/plugins/picerne-google-maps/img/gmap_pin.png",
+					iconsize: [20,20],
+					iconanchor: [20,20]
+				}
+			},
+			{
+			    latitude: 27.9287741,
+				longitude: -82.3353131,
+				html: "<strong>Brandon Mall</strong><p>459 Brandon Town Center Dr<br>Brandon, FL 33511</p>",
+				icon: {
+					image: "/plugins/picerne-google-maps/img/gmap_pin.png",
+					iconsize: [20,20],
+					iconanchor: [20,20]
+				}
+			}												
 									
 		],
 		animation: "BOUNCE"		
